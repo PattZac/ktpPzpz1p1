84589fcb9ff445a5bade917aa9ff74918193afb0
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-22 15:33:12 +0100

    Add files via upload

---
+++ ktpPzpz1p1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ktpPzpz1p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite neku rečenicu: ");
+            string s = Console.ReadLine();
+            if (s.Length < 5) {
+                Console.WriteLine(s);
+            }
+            else
+            {
+                Console.WriteLine(s.Substring(2, s.Length - 4));
+            }
+            Console.ReadKey();
+        }
+    }
+}
